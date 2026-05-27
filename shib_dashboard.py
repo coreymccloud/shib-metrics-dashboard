d984690d738c2b4987db2c52536f21fbb4fed9bf
coreymccloud <156555686+coreymccloud@users.noreply.github.com>
2026-05-26 20:45:53 -0400

    Update shib_dashboard.py

--- shib_dashboard.py
+++ shib_dashboard.py
@@ -67,7 +67,7 @@ def fetch_supply_and_burn():
             burned += int(bal_resp.get('result', 0))
         
         initial_supply = 1_000_000_000_000_000
-        burn_percentage = round((burned / initial_supply), 2)
+        burn_percentage = round((burned / 100), 2)
         
         return {
             "total_supply": total_supply,
